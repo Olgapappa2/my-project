de65d5185b895af073c9939c08f3ab3cb846a283
Olgapappa2 <olga-pappa@hotmail.com>
2025-05-21 01:18:15 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.helloapp;
+/**
+ * This project was created to show how to:
+ * 1. Make a git project.
+ * 2. Stage and commit changes in local repo.
+ * 3. Push to GitHub.
+ */
+public class HelloWorld {
+}
